a5ddc02b54c101caa268ed544d45a4aac78efd5f
Tim Popham <popham@uw.edu>
2014-06-11 12:21:06 -0700

    Use amdefine for node environment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-exports.Store = require('./lib/Store');
-exports.Act = require('./lib/Act');
-exports.Publish = require('./lib/Publish');
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
+
+define(['./lib/Store', './lib/Act', './lib/Publish'], function (
+               Store,         Act,         Publish) {
+    return {
+        Store : Store,
+        Act : Act,
+        Publish : Publish
+    };
+});
