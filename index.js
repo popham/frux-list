99d360bca5123f797c5b67e1e9a953724bc46efb
Tim Popham <popham@uw.edu>
2014-06-11 16:50:02 -0700

    Dumped amdefine

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
-
 define(['./lib/Store', './lib/Act', './lib/Publish'], function (
                Store,         Act,         Publish) {
     return {
