6bd052ba87644575cf91b91dc57e8a472328e432
Tim Popham <popham@uw.edu>
2014-06-17 15:38:29 -0700

    Syntax fix

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 define(['./lib/Publish', './lib/List', './lib/Store', './lib/act/index'], function (
                Publish,         List,         Store,         act) {
     return {
-        List : List
+        List : List,
         Publish : Publish,
         Store : Store,
         act : act
