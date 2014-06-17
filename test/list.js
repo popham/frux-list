2f27615186800ca948ca09d15f530239d2e15bc8
Tim Popham <popham@uw.edu>
2014-06-17 11:40:19 -0700

    Refactor to sparse list

--- test/list.js
+++ test/list.js
@@ -4,18 +4,19 @@ var assert = require('assert');
 describe('Appended items', function () {
     it('should not invalidate iterators', function () {
         var s = new Store();
-        s.append([0,1,2,3]);
+
+        s.act.append.push([0,1,2,3]);
         var oldEnd = s.last;
-        s.append([4,5,6]);
-        
+        s.act.append.push([4,5,6]);
+
         assert(oldEnd.hasNext, "Iterator refers to store state");
     });
 
     it('should appear at the end of all items', function () {
         var s = new Store();
-        s.append([0,1,2,3]);
+        s.act.append.push([0,1,2,3]);
         var oldEnd = s.last;
-        s.append([4]);
+        s.act.append.push([4]);
 
         assert.strictEqual(s.last.value, 4);
         assert.strictEqual(s.first.value, 0);
@@ -25,13 +26,13 @@ describe('Appended items', function () {
 describe('Item splicing', function () {
     it('should not disrupt iterator validity when unnecessary', function () {
         var s = new Store();
-        s.append([0,1,2,3,4]);
+        s.act.append.push([0,1,2,3,4]);
         var mid = s.first.increment().increment();
 
-        s.splice(mid.key, 0, [10,11,12]);
+        s.act.splice.push(mid.key, 0, [10,11,12]);
         assert.strictEqual(mid.value, 2);
 
-        s.splice(mid.key, 1, [20,21,22]);
+        s.act.splice.push(mid.key, 1, [20,21,22]);
         assert.throws(function () { return mid.value; }, TypeError);
         assert.strictEqual(mid.hasNext, false);
         assert.strictEqual(mid.hasPrior, false);
@@ -39,75 +40,75 @@ describe('Item splicing', function () {
 
     it('should inject items into the middle', function () {
         var s = new Store();
-        s.append([0,1,2,3,4]);
+        s.act.append.push([0,1,2,3,4]);
         var mid = s.first.increment().increment();
-        s.splice(mid.key, 0, [10,11,12]);
+        s.act.splice.push(mid.key, 0, [10,11,12]);
 
-        assert.deepEqual([0, 1, 10, 11, 12, 2, 3, 4], s.items.values);
+        assert.deepEqual([0, 1, 10, 11, 12, 2, 3, 4], s.values);
     });
 
     it('should overwrite a larger set of items', function () {
         var s = new Store();
-        s.append([0,1,2,3,4]);
+        s.act.append.push([0,1,2,3,4]);
         var mid = s.first.increment();
-        s.splice(mid.key, 3, [10]);
+        s.act.splice.push(mid.key, 3, [10]);
 
-        assert.deepEqual([0, 10, 4], s.items.values);
+        assert.deepEqual([0, 10, 4], s.values);
     });
 
     it('should overwrite a smaller set of items', function () {
         var s = new Store();
-        s.append([0,1,2,3,4]);
+        s.act.append.push([0,1,2,3,4]);
         var mid = s.first.increment();
-        s.splice(mid.key, 2, [10, 11, 12, 13]);
+        s.act.splice.push(mid.key, 2, [10, 11, 12, 13]);
 
-        assert.deepEqual([0, 10, 11, 12, 13, 3, 4], s.items.values);
+        assert.deepEqual([0, 10, 11, 12, 13, 3, 4], s.values);
     });
 
     it('should remove a set of items', function () {
         var s = new Store();
-        s.append([0,1,2,3,4]);
+        s.act.append.push([0,1,2,3,4]);
         var mid = s.first.increment();
-        s.splice(mid.key, 3, []);
+        s.act.splice.push(mid.key, 3, []);
 
-        assert.deepEqual([0, 4], s.items.values);
+        assert.deepEqual([0, 4], s.values);
     });
 
     it('should overwrite the same size set of items', function () {
         var s = new Store();
-        s.append([0,1,2,3,4]);
+        s.act.append.push([0,1,2,3,4]);
         var mid = s.first.increment();
-        s.splice(mid.key, 3, [10, 11, 12]);
+        s.act.splice.push(mid.key, 3, [10, 11, 12]);
 
-        assert.deepEqual([0, 10, 11, 12, 4], s.items.values);
+        assert.deepEqual([0, 10, 11, 12, 4], s.values);
     });
 
     it('should overwrite the tail', function () {
         var s = new Store();
-        s.append([0,1,2,3,4]);
+        s.act.append.push([0,1,2,3,4]);
         var mid = s.first.increment().increment().increment();
-        s.splice(mid.key, 3, [10, 11, 12]);
+        s.act.splice.push(mid.key, 3, [10, 11, 12]);
 
-        assert.deepEqual([0, 1, 2, 10, 11, 12], s.items.values);
+        assert.deepEqual([0, 1, 2, 10, 11, 12], s.values);
     });
 });
 
 describe('Item removal', function () {
     it('should remove however many are available at the tail without error', function () {
         var s = new Store();
-        s.append([0,1,2,3,4]);
+        s.act.append.push([0,1,2,3,4]);
         var mid = s.first.increment().increment().increment();
-        s.remove(mid.key, 5);
+        s.act.remove.push(mid.key, 5);
 
-        assert.deepEqual([0, 1, 2], s.items.values);
+        assert.deepEqual([0, 1, 2], s.values);
     });
 
     it('should remove items from the middle', function () {
         var s = new Store();
-        s.append([0,1,2,3,4]);
+        s.act.append.push([0,1,2,3,4]);
         var mid = s.first.increment();
-        s.remove(mid.key, 3);
+        s.act.remove.push(mid.key, 3);
 
-        assert.deepEqual([0, 4], s.items.values);
+        assert.deepEqual([0, 4], s.values);
     });
 });
